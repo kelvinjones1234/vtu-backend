c08cc84e75d10762e38cd18c916a8cbefce0ca00
kelvinjones1234 <praisejournal2@gmail.com>
2024-08-04 15:13:02 +0100

    updated

--- users/serializers.py
+++ users/serializers.py
@@ -10,6 +10,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
         token['first_name'] = user.first_name
         token['phone_number'] = user.phone_number
+
         return token
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -18,7 +19,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name', 'username', 'password', 'confirm_password', 'phone_number', 'transaction_pin']
+        fields = ['email', 'username', 'password', 'confirm_password', 'phone_number', 'transaction_pin']
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
