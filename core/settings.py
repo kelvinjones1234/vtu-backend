c583e7d2c91346ceb217fb3e6013e5bcfc461958
kelvinjones1234 <praisejournal2@gmail.com>
2024-08-06 13:56:36 +0100

    updated

--- core/settings.py
+++ core/settings.py
@@ -19,20 +19,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
-    'users',
-    'transactions',
-    'products',
-    'django_ulid',
-    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'transactions',
+    'products',
+    'django_ulid',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
